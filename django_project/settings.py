5afc32f16a4da79cc0425b58c883816bfc889f9f
wxndxx <47880071+wxndxx@users.noreply.github.com>
2024-03-01 18:42:12 +0300

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "appsshouldwork.twc1.net"]
+ALLOWED_HOSTS = ["*"]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = [
